8a44692b22ded1e5844252a8ac7c5f08498fd41c
maxxh2 <122550094+maxxh2@users.noreply.github.com>
2023-02-16 08:58:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Name: Maxx Hernandez
+ * Date: 02/16/2023
+ * Description: C# Deliverable(5) Using two methods
+ 
+
+*/
+namespace deliverable_5_Methods
+{
+    class Program
+    {
+        static int[] random_array(int array_len)
+        {
+            
+            Random random= new Random();
+            int[] MyArray = new int[array_len];
+            for(int i = 0; i < array_len; i++)
+            {
+                MyArray[i] = random.Next(10,50); 
+            }
+            return MyArray;
+        }
+
+        static int sum(int[] array)
+        {
+            int sum = 0;
+            foreach(int item in array)
+            {
+                sum = sum + item;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int input = 0;
+                while (input < 5 || input > 15)
+                {
+                    Console.WriteLine("Enter an integer between 5 and 15: ");
+                    input = int.Parse(Console.ReadLine());
+                    if (input < 5 || input > 15)
+                    {
+                        Console.WriteLine("Invalid input. Please enter an integer between 5 and 15.");
+                    }
+                }
+
+                int[] sampleArray = random_array(input);
+
+                Console.WriteLine("The elements in the random array are: ");
+
+                foreach (int element in sampleArray)
+                {
+                    Console.Write(element + " ");
+                }
+
+                Console.WriteLine(" ");
+                Console.WriteLine("The sum is: " + sum(sampleArray));
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer (whole number)");
+            }
+        }
+    }
+}
